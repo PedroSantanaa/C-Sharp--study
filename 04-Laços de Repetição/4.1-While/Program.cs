5228e55d7e2af721d285badd76555528ec358647
Pedro Santana <pedro.ferraz01@gmail.com>
2023-03-26 13:28:03 -0300

    While and For

---
+++ 04-Laços de Repetição/4.1-While/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Caracteres{
+
+  static void Main(string[] args){
+    double investimento=1000;
+
+    //rendimento 0.5% ao mes
+    double rendimento=0;
+    // rendimento=investimento*0.005;
+    // investimento+=rendimento;
+    // Console.WriteLine("Seu investimento rendeu: R$"+rendimento+" "+"e no total voce tem R$"+investimento);
+
+    //Para 12 meses
+    int mes=1;
+    while(mes<=12){
+      rendimento=investimento*0.005;
+      investimento+=rendimento;
+      mes+=1;
+    }
+    Console.WriteLine("Seu investimento rendeu por "+(mes-1)+" meses "+"e no total voce tem R$"+investimento);
+  
+  }
+}
---
+++ 04-Laços de Repetição/4.2-For/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Caracteres{
+
+  static void Main(string[] args){
+    double investimento=1000;
+    double rendimento=0;
+
+    //Para 12 meses
+    for(int mes=1;mes<=12;mes++){
+      rendimento=investimento*0.005;
+      investimento+=rendimento;
+      Console.WriteLine("Seu investimento rendeu por "+(mes)+" meses "+" "+"com rendimento de R$"+rendimento+" e no total voce tem R$"+investimento);
+    }
+    
+  
+  }
+}
