47e0ef8a6e338c20ac03712ae8a371a40d691777
Pedro Santana <pedro.ferraz01@gmail.com>
2023-03-26 22:13:52 -0300

    Class  and methods

---
+++ Programação orientada a objetos em C#/01-bitebank/ContaCorrente.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_bitebank
+{
+    public class ContaCorrente
+    {
+        public int numeroAgencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+        
+        public void Depositar(double valor){
+            this.saldo+=valor;
+        }
+        public bool Saque(double valor){
+            if(valor<=this.saldo){
+                this.saldo-=valor;
+                return true;
+                }
+                else{
+                    
+                    return false;
+                }
+            
+        }
+
+        public bool Transferir(double valor,ContaCorrente destino){
+            if(this.saldo<valor){
+                return false;
+            }else{
+                this.Saque(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+
+        }
+        public void Show(){
+           Console.WriteLine("Informações da conta: "+this.titular+ " "+ this.numeroAgencia+" "+this.conta+" "+this.saldo);
+        }
+    }
+}
---
+++ Programação orientada a objetos em C#/01-bitebank/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _01_bitebank;
+ContaCorrente contaDoAndre= new ContaCorrente();
+contaDoAndre.titular="Andre Silva";
+contaDoAndre.numeroAgencia=15;
+contaDoAndre.conta="1010-x";
+contaDoAndre.saldo=100;
+
+Console.WriteLine("O saldo de André é R$"+contaDoAndre.saldo);
+
+contaDoAndre.Depositar(100);
+Console.WriteLine("O saldo de André é R$"+contaDoAndre.saldo);
+if(contaDoAndre.Saque(300)==true){
+  Console.WriteLine("O saldo de André é R$"+contaDoAndre.saldo);
+}else{
+  Console.WriteLine("Saldo insuficiente");
+};
+
+ContaCorrente contaDaMaria=new ContaCorrente();
+contaDaMaria.titular="Maria Silva";
+contaDaMaria.numeroAgencia=23;
+contaDaMaria.conta="1212-x";
+contaDaMaria.saldo=50;
+
+if(contaDoAndre.Transferir(50,contaDaMaria)){
+  Console.WriteLine("O saldo de André é R$"+contaDoAndre.saldo);
+  Console.WriteLine("O saldo de Maria é R$"+contaDaMaria.saldo);
+}else{
+  Console.WriteLine("Saldo insuficiente de André");
+  
+}
+
+contaDoAndre.Show();
+
