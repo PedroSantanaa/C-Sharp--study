cf69d350651558de8c66b4fc419a92833a83cc5f
pedro santana <pedro.ferraz01@gmail.com>
2023-05-31 21:44:47 -0300

    Finishing collections

---
+++ Collections2/ArrayMultidimensional/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[,] resultados = new string[3, 3]
+        {
+                {"Alemanha", "Espanha", "Itália"},
+                {"Argentina", "Holanda", "França"},
+                {"Holanda", "Alemanha", "Alemanha"},
+        };
+
+        //Para fazer como se fosse tabela
+        for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
+        {
+            int ano = 2014 - copa * 4;
+            Console.Write(ano.ToString().PadRight(12)  );
+        }
+        Console.WriteLine();
+        for (int posicao = 0; posicao <= resultados.GetUpperBound(0); posicao++)
+        {
+            for (int copa = 0; copa <= resultados.GetUpperBound(1); copa++)
+            {
+                Console.Write(resultados[posicao, copa].PadRight(12));
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+
+}
---
+++ Collections2/JaggedArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //família 1: Família Flinstones
+        //família 2: Família Simpsons
+        //família 3: Família Dona Florinda
+
+        //JAGGED ARRAYS
+
+        string[][] familias = new string[3][];
+        familias[0] = new string[] { "Fred", "Vilma", "Pedrita" };
+        familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Maggie" };
+        familias[2] = new string[] { "Florinda", "Kiko" };
+
+        //Para printar
+
+        foreach (var s in familias)
+        {
+            Console.WriteLine(string.Join(",",s));
+        }
+
+    }
+}
