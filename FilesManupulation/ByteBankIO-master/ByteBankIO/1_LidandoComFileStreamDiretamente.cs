9d5c89b5459ce3faaeb951d92dcaac126f38c630
pedro santana <pedro.ferraz01@gmail.com>
2023-06-01 17:00:39 -0300

    Manipulação de Arquivos FileStream

---
+++ FilesManupulation/ByteBankIO-master/ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,43 @@
+﻿using ByteBankIO;
+using System.Text;
+using System.Text.Unicode;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente(string[] args)
+    {
+        var pathFile = "contas.txt";
+        using (var fluxoDoArquivo = new FileStream(pathFile, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+
+
+            var buffer = new byte[1024];//1KB
+
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+
+            }
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
+        }
+    }
+
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+}
---
+++ FilesManupulation/ByteBankIO-master/ByteBankIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using ByteBankIO;
+using System.Text;
+using System.Text.Unicode;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using(var fluxoDeArquivo=new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor=new StreamReader(fluxoDeArquivo);
+            //Linha unica
+           // var linha=leitor.ReadLine();
+            //Console.WriteLine(linha);
+            //Arquivo completo
+            //var texto=leitor.ReadToEnd();
+            //Console.WriteLine(texto);
+            //Linha a linha
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
+       Console.ReadLine();
+    }
+        
+}
