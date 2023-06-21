3eead804d2411c236e36341205d38cad6bd3fb8f
pedro santana <pedro.ferraz01@gmail.com>
2023-06-20 21:44:39 -0300

    feat:POST GET PUT DELETE endpoints

--- IWantApp/src/Program.cs
+++ IWantApp/src/Program.cs
@@ -1,4 +1,8 @@
+using IWantApp.Endpoints.Categories;
+using IWantApp.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,28 +20,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+//POST
+app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
+//GET ALL
+app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
+//PUT
+app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+//DELETE
+app.MapMethods(CategoryDelete.Template, CategoryDelete.Methods, CategoryDelete.Handle);
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
